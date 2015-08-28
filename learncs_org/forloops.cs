c209b9e8a1b6150d1bb32b7457b024d445e13e85
azam alias <azam.alias@nintex.com>
2015-08-28 09:46:12 +0800

    for loops

---
+++ learncs_org/forloops.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        string x = "Hi";
+        int n = 10;
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
