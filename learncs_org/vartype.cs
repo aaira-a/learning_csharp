fc69846f81bed11a8ddfc07a0ea89d9ca006e081
azam alias <azam.alias@nintex.com>
2015-08-28 08:23:02 +0800

    variables and types

---
+++ learncs_org/vartype.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        string productName = "TV";
+        int productYear = 2012;
+        float productPrice = 279.99f;
+
+        Console.WriteLine("productName: " + productName);
+        Console.WriteLine("productYear: " + productPrice);
+        Console.WriteLine("productPrice: " + productPrice);
+    }
+}
