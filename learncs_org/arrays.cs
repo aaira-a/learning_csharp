d53faf3b5ef6408bc0b6d8e4f536450aacfd971f
azam alias <azam.alias@nintex.com>
2015-08-28 08:27:11 +0800

    arrays

---
+++ learncs_org/arrays.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        string[] fruits = {"apple", "banana", "orange"};
+
+        Console.WriteLine(fruits[0]);
+        Console.WriteLine(fruits[1]);
+        Console.WriteLine(fruits[2]);
+    }
+}
