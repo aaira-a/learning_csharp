f0455fa1b3fdf8939acc97fcddd75cad73953f80
azam alias <azam.alias@nintex.com>
2015-08-28 09:57:01 +0800

    while loops

---
+++ learncs_org/whileloops.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        string x = "Hi";
+        int n = 10;
+
+        int i = 0;
+        while(i < n)
+        {
+            Console.WriteLine(x);
+            i++;
+        }
+    }
+}
